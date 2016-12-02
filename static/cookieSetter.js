ec7e4826bf250f79e2f1bcc673487ec53638a0af
ClonedOne <severi.giorgio@gmail.com>
2016-12-02 20:26:52 +0100

    refactor

--- static/cookieSetter.js
+++
@@ -1,4 +0,0 @@
-document.cookie =
-  "testcookie=" + encodeURIComponent("my test") +
-  "; max-age=" + 60*60*24*30 +
-  "; path=/; domain=local.com" ; 
--- static/secondary.js
+++
@@ -1,4 +0,0 @@
-console.log('Secondary javascript')
-console.log('setting the domain')
-//document.domain = "local.com";
-
