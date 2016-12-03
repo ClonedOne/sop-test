57a82cbd9f7969b278be122c25d80fc6cdb29940
ClonedOne <severi.giorgio@gmail.com>
2016-12-03 01:12:43 +0100

    refactoring

--- honest/static/primary.js
+++ honest/static/primary.js
@@ -1,6 +1,6 @@
 console.log('Primary javascript')
 console.log('setting the domain')
-//document.domain = "local.com";
+document.domain = "local.com";
 console.log('opening new window');
 var windowObjectReference = window.open('http://local.com:5000', 'newindow');
 var mydiv = windowObjectReference.document.getElementById('main_page');
